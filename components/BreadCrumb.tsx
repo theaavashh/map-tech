4ff91acd6f5772bfc1a30cc84ef05a4bade50170
theaavashh <aavash.ganeju@gmail.com>
2025-01-22 12:08:32 +0545

    feat (breadcrumb): add breadcrumb component

---
+++ components/BreadCrumb.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type BCType = {
+  title: string;
+  desc: string;
+};
+
+const BreadCrumb = ({ title, desc }: BCType) => {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-44 md:h-54 bg-[#0E1F51] gap-3">
+      <h2 className="text-white text-xl md:text-3xl">{title}</h2>
+      <h3 className="text-white text-lg md:text-2xl">{desc}</h3>
+    </div>
+  );
+};
+
+export default BreadCrumb;
