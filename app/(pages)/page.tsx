e9494403d737edd1813df9819b322cebc06a04ea
theaavashh <aavash.ganeju@gmail.com>
2025-01-22 10:56:55 +0545

    feat (page): update home page with herosection component

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,10 +1,13 @@
+import HeroSection from '@/components/HeroSection';
 import Navbar from '@/components/Navbar';
 import React from 'react';
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+      <div className="mt-5">
+        <HeroSection />
+      </div>
     </>
   );
 };
