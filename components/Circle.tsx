62b3dcf112c886266bcf90445e35212478123b30
theaavashh <aavash.ganeju@gmail.com>
2025-01-22 10:45:35 +0545

    feat (circle) : add circle component code

---
+++ components/Circle.tsx
@@ -0,0 +1,17 @@
+/*
+import package from react
+*/
+import React from 'react';
+
+/*
+Execute the circle component code
+*/
+const Circle = () => {
+  return (
+    <div>
+      <div className="rounded-full h-40 w-40  bg-[#e5f0ff] mt-4 "></div>
+    </div>
+  );
+};
+
+export default Circle;
